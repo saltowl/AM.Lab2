2387a638ab7cd8f1c4b509ba7e447fa3d3883192
saltowl <vladislava-shakirova@mail.ru>
2019-03-10 20:29:49 +0300

    Correct code style

--- AM.Lab2/AM.Lab2.py
+++ AM.Lab2/AM.Lab2.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
 def simulation_Markov_chain (matrix, pi, eps):
 	standard_deviation = np.inf
@@ -21,7 +22,7 @@ def simulation_Markov_chain (matrix, pi, eps):
 
 	plot_graph_of_standard_deviation(x, y, pi)
 
-	return result
+	return (result, steps_count - 1)
 
 def calculate_standard_deviation (v1, v2):
 	res = 0
@@ -32,8 +33,7 @@ def calculate_standard_deviation (v1, v2):
 
 def simulation_Markov_chain_analytically (matrix, pi):
 	a = matrix.transpose() - np.identity(len(matrix))
-	norm = np.ones(len(matrix))
-	a[len(matrix) - 1] = norm
+	a[len(matrix) - 1] = np.ones(len(matrix))
 
 	b = np.zeros(len(matrix))
 	b[len(matrix) - 1] = 1
@@ -48,8 +48,15 @@ def plot_graph_of_standard_deviation (x, y, pi):
 	plt.title('Change in standard deviation')
 	plt.legend()
 	pass
+
+def print_result_of_simulation_Markov_chain(matrix, pi, eps):
+	print('\nπ = ', pi)
+	res = simulation_Markov_chain(matrix, pi, eps)
+	print('P (', res[1], ') = ', res[0])
+	pass
+
 def main():
-	matrix = np.array([[0.2, 0, 0.4, 0.1, 0, 0, 0.15, 0.15],
+	transition_matrix = np.array([[0.2, 0, 0.4, 0.1, 0, 0, 0.15, 0.15],
 					[0, 0.1, 0, 0.2, 0.5, 0, 0.2, 0],
 					[0.1, 0, 0.1, 0, 0, 0.4, 0.3, 0.1],
 					[0, 0.1, 0, 0.3, 0, 0, 0.3, 0.3], 
@@ -58,22 +65,21 @@ def main():
 					[0.1, 0.2, 0.05, 0.1, 0, 0.2, 0.05, 0.3],
 					[0.2, 0.3, 0.1, 0.1, 0, 0.2, 0, 0.1]])
 	
-	print('Source transition probability matrix')
-	print(matrix)
+	print('\nSource transition probability matrix')
+	print(transition_matrix)
 
 	eps = 0.000000001
 	print('\n\nThe vectors of the distribution over the states obtained numerically with precision: ', eps)
 	pi0 = [1, 0, 0, 0, 0, 0, 0, 0]
-	print('\nFor initial state vector: ', pi0)
-	print(simulation_Markov_chain(matrix, pi0, eps))
 	pi1 = [0, 0, 1, 0, 0, 0, 0, 0]
-	print('\nFor initial state vector: ', pi1)
-	print(simulation_Markov_chain(matrix, pi1, eps))
 	pi2 = [0.25, 0, 0.25, 0, 0.5, 0, 0, 0]
-	print('\nFor initial state vector: ', pi2)
-	print(simulation_Markov_chain(matrix, pi2, eps))
+	print_result_of_simulation_Markov_chain(transition_matrix, pi0, eps)
+	print_result_of_simulation_Markov_chain(transition_matrix, pi1, eps)
+	print_result_of_simulation_Markov_chain(transition_matrix, pi2, eps)
 
 	print('\n\nThe vector of the distribution over the states obtained analytically')
+	print(simulation_Markov_chain_analytically(transition_matrix, pi0))
+
 	plt.show()
 	pass
 
